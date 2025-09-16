eb06a275beb0ffbdc4ce3f07a82b7b98ed3af76a
Hugo <ht0981.2005@gmail.com>
2025-09-16 11:02:28 -0300

    feat: Atualização da resposta da API
    Agora a API classifica as reviews e permite algumas configurações na requisição

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -21,16 +21,39 @@ def lambda_handler(event, context):
     vectorizer = joblib.load(config.VECTORIZER_PATH)
 
     body = json.loads(event["body"])
+    review_title = body.get("review_title", "")
     review_text = body["review_text"]
 
-    processed_review = utils.process_text(review_text)
+    review_full_text = review_title + " " + review_text
+
+    processed_review = utils.process_text(review_full_text)
     text_vector = vectorizer.transform([processed_review])
 
     predicted_score = model.predict(text_vector)[0]
 
-    response = {
-        "review_score": predicted_score
-        }
+    allow_neutral = body.get("allow_neutral", False)
+    show_score = body.get("show_score", True)
+    show_full_score = body.get("show_full_score", False)
+
+    classification = ""
+
+    if allow_neutral:
+        if predicted_score < 0.4:
+            classification = "negative"
+        elif predicted_score < 0.6:
+            classification = "neutral"
+        else:
+            classification = "positive"
+    else:
+        classification = "negative" if predicted_score < 0.5 else "positive"
+
+    response = {"classification": classification}
+    
+    if show_score:
+        if show_full_score:
+            response["score"] = predicted_score
+        else:
+            response["score"] = float(f"{predicted_score:.2f}")
 
     return {
         'statusCode': 200,
